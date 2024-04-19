32a3c3baf7cc54aec4f6c4cae297888e8f6c25a7
Jenifer Scarlate <jenifer.scarlate@unesp.br>
2024-04-19 09:43:30 -0300

    script: desenvolvendo o CREATE do CRUD

--- script.js
+++ script.js
@@ -6,6 +6,29 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active');
 }
 
+
+//CRUD - create, read, update, delete
+
+//Create
+
+const tempUser = {
+    nome: 'ana',
+    email: 'ana-jenifer@gmail.com',
+    tel: '11940247364',
+    cidade: 'Botucatu'   
+}
+
+const setLocalStorege = (dbUser) => localStorage.setItem('db_User', JSON.stringify(dbUser)); 
+
+const getLocalStorege = () => JSON.parse(localStorage.getItem('db_User')) ?? [];
+
+
+const creatUser = (user) => {
+    const dbUser = getLocalStorege();
+    dbUser.push(user);
+    setLocalStorege(dbUser);
+}
+
 document.getElementById('cadastrarUsuario').addEventListener('click', openModal);
 
 document.getElementById('modalClose').addEventListener('click', closeModal);
